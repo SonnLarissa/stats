7bb2c3a374e5846ec867a963d4340cb5f080a49d
Larisa Kasyanova <lkasyanova@velox.biz>
2021-04-06 10:32:39 +0500

    added CategoriesAvg

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -1,6 +1,8 @@
 package stats
 
-import "github.com/SonnLarissa/bank/pkg/types"
+import (
+	"github.com/SonnLarissa/bank/pkg/types"
+)
 
 // Avg рассчитывает среднюю сумму платежа.
 func Avg(payments []types.Payment) types.Money {
@@ -19,4 +21,51 @@ func TotalInCategory(payments []types.Payment, category types.Category) types.Mo
 		sum += p.Amount
 	}
 	return sum
+}
+
+//FilterByCategory возвращяет платежи в указаной категории
+func FilterByCategory(payments []types.Payment, category types.Category) []types.Payment {
+	var filtered []types.Payment
+	for _, payment := range payments {
+		if payment.Category == category {
+			filtered = append(filtered, payment)
+		}
+	}
+	return filtered
+}
+
+//CategoriesTotal возвращает сумму платежей по каждой категории
+func CategoriesTotal(payments []types.Payment) map[types.Category]types.Money {
+	categories := map[types.Category]types.Money{}
+	for _, payment := range payments {
+		categories[payment.Category] += payment.Amount
+	}
+	return categories
+}
+
+func CategoriesAvg(payments []types.Payment) map[types.Category]types.Money {
+	r := make(map[types.Category]types.Money)
+	filter := make(map[types.Category][]types.Payment)
+	for _, payment := range payments {
+		if _, res := filter[payment.Category]; res {
+			filter[payment.Category] = append(filter[payment.Category], payment)
+		} else {
+			filter[payment.Category] = []types.Payment{payment}
+		}
+	}
+	f := func(payments []types.Payment) (types.Money, int) {
+		count := 0
+		m := types.Money(0)
+		for _, payment := range payments {
+			count++
+			m += payment.Amount
+		}
+		return m, count
+	}
+
+	for k, v := range filter {
+		m, c := f(v)
+		r[k] = m / types.Money(c)
+	}
+	return r
 }
